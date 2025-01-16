99ce5d9a50d320c360ab714a71e60fd10be036d2
George-Oncy <gorg.oncy88@gmail.com>
2025-01-16 14:52:35 +0200

    Update Card.jsx

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -4,20 +4,20 @@ import { useState } from "react";
 
 
 
-export default function Card(img) {
+export default function Card(props) {
     const [isShow, setIsShow] = useState(false);
 
   return (
     <>
       <div
         className="w-full md:w-1/2 lg:w-1/3 p-5 relative "
-        onClick={() => setIsShow(img)}
+        onClick={() => setIsShow(props.img)}
       >
         <div className="inner relative group/lay ">
           <div className="layer bg-teal-500 bg-opacity-80 group-hover/lay:transition-all hidden  group-hover/lay:flex group-hover/lay:duration-1000 duration-1000  absolute left-0 top-0 right-0 bottom-0 rounded-lg  justify-center items-center">
             <i className="fa-solid opacity-100 fa-plus text-8xl text-white"></i>
           </div>
-          <img src={img} className="w-full rounded-lg" alt="" />
+          <img src={props.img} className="w-full rounded-lg" alt="" />
         </div>
       </div>
 
